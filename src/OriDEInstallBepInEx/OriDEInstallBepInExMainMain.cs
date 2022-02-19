85251c2f07fa047af15331537d6fa9f5b1fa7cd9
FurryRbl <VupRbl@gmail.com>
2022-02-19 17:13:32 +0800

    开发中

---
+++ src/OriDEInstallBepInEx/OriDEInstallBepInExMainMain.cs
@@ -0,0 +1,13 @@
+﻿namespace OriDEInstallBepInEx
+{
+    public class OriDEInstallBepInExMainMain
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("安装程序启动!");
+            ApplicationConfiguration.Initialize();
+            Application.Run(new GuiMain());
+        }
+    }
+}
