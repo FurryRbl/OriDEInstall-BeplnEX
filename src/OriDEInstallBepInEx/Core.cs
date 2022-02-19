b0c3871fc892ddafbdf761cadffd3e89a0f77729
FurryRbl <VupRbl@gmail.com>
2022-02-19 17:12:47 +0800

    未开发完成

---
+++ src/OriDEInstallBepInEx/Core.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OriDEInstallBepInEx
+{
+    public static class Core
+    {
+        public static void Install()
+        {
+            Console.WriteLine("选择程序位置...");
+            System.Windows.Forms.FolderBrowserDialog Game = new System.Windows.Forms.FolderBrowserDialog();
+            //Game.Description = "选择游戏目录";
+            Game.ShowNewFolderButton = true;
+            Game.RootFolder = Environment.SpecialFolder.ApplicationData;
+            System.Windows.Forms.DialogResult result = Game.ShowDialog();
+            Console.WriteLine("选择路径:" + Game.SelectedPath);
+        }
+    }
+}
